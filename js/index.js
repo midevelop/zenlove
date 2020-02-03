dfe91f117f059d0d58b73cdfd9e463c14bc155bd
midevelop <mi@elbrusboot.camp>
2020-02-03 19:58:38 +0300

    feat(project): logics

--- js/index.js
+++ js/index.js
@@ -1,31 +1,44 @@
-console.log("Hello")
+console.log("Hello");
 
+let stageCreator = (label = "", seconds = 0) => ({ label, seconds });
 
 let app = new Vue({
-    el: "#app",
-    data: {
-        second: 0,
-        stages: ["Вдох", "Задержка", "Выдох"],
-        inhale: 4,
-        hold: 18,
-        exhale: 25,
-        isPause: false
+  el: "#app",
+  data: {
+    second: 0,
+    stages: [
+      stageCreator("Вдох", 4),
+      stageCreator("Задержи", 2),
+      stageCreator("Выдох", 6)
+    ],
+    stage: 0,
+    realTime: 0,
+    circle: 1
+  },
+  created() {
+    this.start();
+  },
+  methods: {
+    start() {
+      setInterval(() => {
+        this.tick();
+      }, 1000);
     },
-    created() {
-        this.start()
+    stop() {
+      console.log("stop");
     },
-    methods: {
-        start() {
-            setInterval(() => {
-                this.tick()
-            }, 1000)
-        },
-        stop() {
-            console.log("stop")
-        },
-        tick() {
-            this.second++
-        },
+    tick() {
+      this.second++;
+      this.realTime--;
+      if (this.realTime < 0) {
 
+        if (this.stage < this.stages.length - 1) this.stage++;
+        else {
+          this.stage = 0;
+          this.circle++;
+        }
+        this.realTime +=  this.stages[this.stage].seconds;
+      }
     }
-})
+  }
+});
